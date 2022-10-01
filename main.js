f86de99dbdfc2f12b0743dd0ed1891b84eaa4347
kimseongtak <kimseongtak1@naver.com>
2022-10-01 15:32:32 +0900

    JS: Arrow Up

--- main.js
+++ main.js
@@ -36,6 +36,20 @@ document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
+// Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add('visible');
+  } else {
+    arrowUp.classList.remove('visible');
+  }
+});
+
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click', () => {
+  scrollIntoView('#home');
+});
 /**
  * selector을 받아서 화면을 selector로 이동시키는 함수
  * @param {*} selector(class(.), id(#) 등을 이용한 Selector)
