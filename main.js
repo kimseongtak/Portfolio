765d88bde97b85e2c7487c2e51b3964775a70703
kimseongtak <kimseongtak1@naver.com>
2022-10-01 15:18:48 +0900

    JS: transparent

--- main.js
+++ main.js
@@ -19,5 +19,27 @@ navbarMenu.addEventListener('click', (event) => {
   if (link == null) {
     return;
   }
-  document.querySelector(link).scrollIntoView({ behavior: 'smooth' });
+  scrollIntoView(link);
 });
+
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home__contact');
+
+homeContactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
+});
+
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+/**
+ * selector을 받아서 화면을 selector로 이동시키는 함수
+ * @param {*} selector(class(.), id(#) 등을 이용한 Selector)
+ */
+const scrollIntoView = (selector) => {
+  document.querySelector(selector).scrollIntoView({ behavior: 'smooth' });
+};
